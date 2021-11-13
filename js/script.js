bdbc707479bef88fcf0e9a50b33402f0830e2866
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-13 21:15:48 +0300

    styling the birthday wishes

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var a=prompt("The UserName is jimmy");
+ var b=prompt("The password is birthday");
+  	if(a== "jimmy" && b=="birthday")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
